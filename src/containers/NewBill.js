829b9bcc9ec788b870b814c80528c12f66c34571
vfauveau <76257448+vfauveau@users.noreply.github.com>
2021-07-03 17:44:40 +0200

    1er commit
    Correction de bug concernant le login de l'administrateur. (fonctionnel).
    
    Ajout d'une fonction de contrôle d'extension de fichiers joint lors de la soumission d'un justificatif pour une nouvelle facture (jpg,jpeg,png).

---
+++ src/containers/NewBill.js
@@ -0,0 +1,76 @@
+
+import { ROUTES_PATH } from '../constants/routes.js'
+import Logout from "./Logout.js"
+
+export default class NewBill {
+  constructor({ document, onNavigate, firestore, localStorage }) {
+    this.document = document
+    this.onNavigate = onNavigate
+    this.firestore = firestore
+    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    formNewBill.addEventListener("submit", this.handleSubmit)
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+    file.addEventListener("change", this.handleChangeFile)
+    this.fileUrl = null
+    this.fileName = null
+    new Logout({ document, localStorage, onNavigate })
+  }
+  handleChangeFile = e => {
+    let file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    let filePath = e.target.value.split(/\\/g)
+    let fileName = filePath[filePath.length-1]
+    // on récupère l'extension et on regarde si le format est le bon
+    let extension = fileName.substring(fileName.lastIndexOf('.') + 1)
+    if(extension.includes("jpg") | extension.includes("jpeg") | extension.includes("png")){
+      this.firestore
+      .storage
+      .ref(`justificatifs/${fileName}`)
+      .put(file)
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        this.fileUrl = url
+        this.fileName = fileName
+      })
+    }
+    else{
+      e.preventDefault()
+      file = ""
+      filePath = ""
+      fileName = ""
+      alert("Le format du justificatif n'est pas supporté, veuillez importer une image au format jpg, jpeg ou png")
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    const email = JSON.parse(localStorage.getItem("user")).email
+    const bill = {
+      email,
+      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+      fileUrl: this.fileUrl,
+      fileName: this.fileName,
+      status: 'pending'
+    }
+    this.createBill(bill)
+    this.onNavigate(ROUTES_PATH['Bills'])
+  }
+
+  // not need to cover this function by tests
+  createBill = (bill) => {
+    if (this.firestore) {
+      this.firestore
+      .bills()
+      .add(bill)
+      .then(() => {
+        this.onNavigate(ROUTES_PATH['Bills'])
+      })
+      .catch(error => error)
+    }
+  }
+}
